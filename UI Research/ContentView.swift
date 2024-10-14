17da4aa2a8f95802ced1d423fc136d317840a651
ramdejin <149199649+ramdejin@users.noreply.github.com>
2024-10-13 21:45:53 -0700

    File after starter app struct
    Application content view page

---
+++ UI Research/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Capstone_WebAPP
+//
+//  Created by Ram Menon on 10/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Welcome to our Capstone Web-APP ! (ASL Features release soon)")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
