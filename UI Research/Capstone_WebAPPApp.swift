037bed71aaded381c1a81517faaf624260a4a410
ramdejin <149199649+ramdejin@users.noreply.github.com>
2024-10-13 21:45:19 -0700

    starter app file
    main file refering to the contentview struct

---
+++ UI Research/Capstone_WebAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capstone_WebAPPApp.swift
+//  Capstone_WebAPP
+//
+//  Created by Ram Menon on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Capstone_WebAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
